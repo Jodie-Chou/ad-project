e901a7cf4c0a7aaf812693d92e17f349b171be73
Jode_Chou <1062680775@qq.com>
2022-08-05 12:01:02 +0800

    eslint相关配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 import './app.scss'
 
 export default class App extends Component {
-	render() {
-		return (
-			<div style={{color: 'red'}}>
-				React Apps
-				<div className="box">
-					<span>哈哈dev分支</span>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div style={{ color: 'red' }}>
+        React Apps
+        <div className="box">
+          <span>哈哈dev分支</span>
+        </div>
+      </div>
+    );
+  }
 }
